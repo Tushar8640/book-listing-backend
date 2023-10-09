0a0f797e97a93ddda3b0052d138a5ec7ff1eb949
Tushar8640 <imrantushar44@gmail.com>
2023-10-10 02:39:05 +0600

    authentication route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.routes';
 
 const router = express.Router();
 
+const moduleRoutes = [
+  {
+    path: '/auth',
+    routes: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
